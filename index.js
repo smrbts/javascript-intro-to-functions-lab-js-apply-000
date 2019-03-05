e7625779aeb01f916b6b339c4e4824ee461ab73c
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-05 03:10:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string){
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string){
