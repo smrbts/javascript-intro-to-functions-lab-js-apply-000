933ad2bd5857e5638d96ea224f0365e42fe0eb6c
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-05 03:57:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string === string.toLowerCase()) return "I can't hear you!";
-  else if(string === "I love you, Grandma.") return ""
+  else if(string === "I love you, Grandma.") return "I love you, too.";
+  else if(string === string.toUpperCase()) return "Yes, indeed!";
 
 }
